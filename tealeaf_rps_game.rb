067483b42b25042ad6999ce50d70b8965989a169
Jeff Millar <jeff.millar@sbcglobal.net>
2015-09-20 00:00:31 -0700

    final commit

--- tealeaf_rps_game.rb
+++ tealeaf_rps_game.rb
@@ -6,19 +6,46 @@
 # 3. Make comparison
 # 4. Display outcome message
 
-puts "Welcome the Rock, Paper, Scissors Game!"
+puts "Welcome to the Rock(r), Paper(p), Scissors(s) Game!"
 
-CHOICES = {r:'Rock', p:'Paper', s:'Scissors'}
+CHOICES = {'r' => 'Rock', 'p' => 'Paper', 's' => 'Scissors'}
+
+def display_winning_message(winning_choice)
+  case winning_choice
+  when 'p'
+    puts "Paper covers rock."
+  when 's'
+    puts "Scissors cuts paper."
+  when 'r'
+    puts "Rock crushes scissors."
+  end
+end
+    
 
 loop do
+  # Player makes selection
   begin
     puts "What is your selection (r, p, or s)?"
     player_choice = gets.chomp.downcase
-  end while CHOICES.keys.has_key?(player_choice)
+    puts "You selected #{player_choice} for #{CHOICES[player_choice]}"
+  end until CHOICES.has_key?(player_choice)
 
-  puts
+  # Computer makes selection
+  computer_choice = CHOICES.keys.sample
+  puts "Computer has selected #{computer_choice} for #{CHOICES[computer_choice]}"
 
+  # Determine outcome and display message
+  if player_choice == computer_choice
+    puts "It's a tie. The computer also selected #{computer_choice}"
+  elsif (player_choice == 'r' && computer_choice == 's') || (player_choice == 'p' && computer_choice == 'r') || (player_choice == 's' && computer_choice == 'p')
+    display_winning_message(player_choice)
+    puts "Congratulations. You win!"
+  else
+    display_winning_message(computer_choice)
+    puts "Computer wins. Sorry."
+  end
 
-
+  puts "Do you want to play again (y/n)?"
+  break if gets.chomp != 'y'
 
 end
